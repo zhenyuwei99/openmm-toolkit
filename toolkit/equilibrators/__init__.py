2fb961dc888f64288c358149fac68b8cb2ae1196
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-06-02 16:09:18 +0800

    Add toolkit.equilibrators package

---
+++ toolkit/equilibrators/__init__.py
@@ -0,0 +1,13 @@
+__author__ = "Zhenyu Wei"
+__maintainer__ = "Zhenyu Wei" 
+__email__ = "zhenyuwei99@gmail.com"
+__copyright__ = "Copyright 2021-2021, Southeast University and Zhenyu Wei"
+__license__ = "GPLv3"
+
+from .equilibrator import Equilibrator
+from .temperatureEquilibrator import TemperatureEquilibrator
+from .pressureEquilibrator import PressureEquilibrator
+
+__all__ = [
+    'TemperatureEquilibrator', 'PressureEquilibrator'
+]
